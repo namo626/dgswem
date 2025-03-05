bd46024d3a248ff17b3873117547c391be5939a9
namo <namo26june@gmail.com>
2025-03-05 14:39:51 -0600

    Add nvtx, remove slopelim MPI

--- work/cmplrflags.mk
+++ work/cmplrflags.mk
@@ -50,7 +50,7 @@ ifeq ($(compiler),nvhpc)   # NVIDIA
   PPFC	        :=  nvfortran
   FC	        :=  nvfortran
   PFC	        :=  mpif90
-  FFLAGS1	:=  -r$(sz) -Mextend -Mlarge_arrays -cuda -traceback -g -O3 -acc -gpu=unified,lineinfo -Minfo=accel
+  FFLAGS1	:=  -r$(sz) -Mextend -Mlarge_arrays -cuda -traceback -g -O3 -acc -gpu=unified,lineinfo -Minfo=accel -lnvhpcwrapnvtx
   #FFLAGS1	:=  -r$(sz) -Mextend -traceback -g -O3 -Minfo=accel
   FFLAGS2	:=  $(FFLAGS1)
   FFLAGS3	:=  $(FFLAGS1)
