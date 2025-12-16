1b084dc0c5b28991cf62c6b55039efe7e243fc53
namo <namo26june@gmail.com>
2025-12-16 08:14:40 -0600

    Work

--- work/cmplrflags.mk
+++ work/cmplrflags.mk
@@ -20,7 +20,7 @@ else ifneq (,$(findstring nvfortran,$(FC)))   # NVIDIA
   PPFC	        :=  nvfortran
   PFC	        :=  mpif90
   ifeq ($(gpu),1)
-	FFLAGS1	:=   -Mextend -r$(sz)   -cuda -traceback -g -O3 -acc -gpu=mem:unified,lineinfo,stacklimit:nostacklimit -Minfo=accel
+	FFLAGS1	:=   -Mextend -r$(sz)   -cuda -traceback -g -O0 -acc -gpu=mem:unified,lineinfo,stacklimit:nostacklimit -Minfo=accel
   else
     FFLAGS1	:=  -Mextend -Mbounds -r$(sz) -traceback -g -O3 -tp=native
   endif
